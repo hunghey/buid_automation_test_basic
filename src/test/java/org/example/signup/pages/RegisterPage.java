b5590568eb8911edc09bb726cf2f111b42c16ae0
hung <nguyenlephuhung15@gmail.com>
2024-10-03 16:24:08 +0700

    update source to trying merge first branch

--- src/test/java/org/example/signup/pages/RegisterPage.java
+++ src/test/java/org/example/signup/pages/RegisterPage.java
@@ -10,24 +10,24 @@ import static com.codeborne.selenide.Selenide.page;
 
 public class RegisterPage {
 
-    @FindBy(id = "floatingInput")
-    private SelenideElement emailInput;
-
-
-    @FindBy(xpath = "//button[text()='Tiếp tục']")
-    private SelenideElement signUpBtn;
-
-    @Step("login account with email and password after register")
-    public RegisterPage inputRegister() {
-        emailInput.setValue("detainhom1101@gmail.com");
-//        emailInput.setValue("p.hung15122002@gmail.com");
-        return this;
-    }
-
-    @Step("click login button")
-    public CodeConfirmationPage clickLogin() {
-        signUpBtn.click();
-        return page(CodeConfirmationPage.class);
-    }
+//    @FindBy(id = "floatingInput")
+//    private SelenideElement emailInput;
+//
+//
+//    @FindBy(xpath = "//button[text()='Tiếp tục']")
+//    private SelenideElement signUpBtn;
+//
+//    @Step("login account with email and password after register")
+//    public RegisterPage inputRegister() {
+//        emailInput.setValue("detainhom1101@gmail.com");
+////        emailInput.setValue("p.hung15122002@gmail.com");
+//        return this;
+//    }
+//
+//    @Step("click login button")
+//    public CodeConfirmationPage clickLogin() {
+//        signUpBtn.click();
+//        return page(CodeConfirmationPage.class);
+//    }
 
 }
